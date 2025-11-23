acf90614df59c56a2fed8b12ddc955ce1e1994f6
Farukh <xfarukh.3@gmail.com>
2025-11-24 00:49:45 +0530

    created interface PRepo extends JpaRepo<P,UUID>

---
+++ Patient-Service/src/main/java/com/pm/Patient_Service/repository/PatientRepository.java
@@ -0,0 +1,12 @@
+package com.pm.Patient_Service.repository;
+
+import com.pm.Patient_Service.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, UUID> {
+
+}
